0dc85d8312e40aab969db8da49f0f54e086143ae
vitaliy-zinchenko <zinjvi@gmail.com>
2019-09-29 20:11:38 -0400

    LC 11

--- src/main/java/zinjvi/leatcode/_11_ContainerWithMostWater.java
+++ src/main/java/zinjvi/leatcode/_11_ContainerWithMostWater.java
@@ -1,4 +1,19 @@
 package zinjvi.leatcode;
 
 public class _11_ContainerWithMostWater {
+    //    class Solution {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            max = Math.max(max, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
 }
