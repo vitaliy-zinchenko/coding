99a2ba966fd6f8b661d049efc5493301266e94db
vitaliy-zinchenko <zinjvi@gmail.com>
2020-01-17 22:52:07 -0500

    some problems

--- src/main/java/zinjvi/leatcode/_188_BestTimeToBuyAndSellStockIV.java
+++ src/main/java/zinjvi/leatcode/_188_BestTimeToBuyAndSellStockIV.java
@@ -5,15 +5,15 @@ import org.junit.Assert;
 public class _188_BestTimeToBuyAndSellStockIV {
 
     public static int maxProfit(int k, int[] prices) {
-        if(prices.length == 0) {
+        if (prices.length == 0) {
             return 0;
         }
 
-        if(k * 2 >= prices.length) {
+        if (k * 2 >= prices.length) {
             int maxProfit = 0;
-            for(int i = 1; i < prices.length; i++) {
-                if(prices[i-1] < prices[i]) {
-                    maxProfit += prices[i] - prices[i-1];
+            for (int i = 1; i < prices.length; i++) {
+                if (prices[i - 1] < prices[i]) {
+                    maxProfit += prices[i] - prices[i - 1];
                 }
             }
             return maxProfit;
@@ -23,24 +23,15 @@ public class _188_BestTimeToBuyAndSellStockIV {
 
         int[][] profit = new int[transactions][prices.length];
         for (int i = 1; i < transactions; i++) {
-            profit[i][0] = 0;
+            int localMax = profit[i - 1][0] - prices[0];
             for (int j = 1; j < prices.length; j++) {
                 int previousDayProfit = profit[i][j - 1];
-                int todaySellProfit = 0;
-                for (int b = 0; b < j; b++) {
-                    todaySellProfit = Math.max(
-                            todaySellProfit,
-                            // Sell it by today's price if I bought it in previous days.
-                            // Which previous day I don't know yet, I will choose by max profit.
-                            // + profit from the previous transaction fro the day I buy current transaction
-                            prices[j] - prices[b] + profit[i - 1][b]
-                    );
-                }
-                profit[i][j] = Math.max(previousDayProfit, todaySellProfit);
+                localMax = Math.max(localMax, profit[i - 1][j-1] - prices[j-1]);
+                profit[i][j] = Math.max(previousDayProfit, prices[j] + localMax);
             }
         }
 
-        return profit[transactions-1][prices.length - 1];
+        return profit[transactions - 1][prices.length - 1];
     }
 
     public static void main(String[] args) {
