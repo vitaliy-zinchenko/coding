d986f37961cb380f31ec4f7978356a2e35cb603f
vitaliy-zinchenko <zinjvi@gmail.com>
2020-02-17 21:04:14 -0500

    some problems

--- src/main/java/zinjvi/bit_manipulations/BinaryToString.java
+++ src/main/java/zinjvi/bit_manipulations/BinaryToString.java
@@ -5,6 +5,9 @@ import org.junit.Assert;
 public class BinaryToString {
 
     public static String convert(double d) {
+        if(d >= 1 || d <= 0) {
+            return  "ERROR";
+        }
         StringBuilder s = new StringBuilder();
         s.append(".");
 
@@ -30,7 +33,6 @@ public class BinaryToString {
         Assert.assertEquals("ERROR", convert(0.72));
         Assert.assertEquals("ERROR", convert(0.3));
         Assert.assertEquals(".101", convert(0.625));
-        Assert.assertEquals(".", convert(0.0));
     }
 
 }
